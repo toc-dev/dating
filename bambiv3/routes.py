a5a6f7cfeb7d46888fbd9414ad04fd4734ccd07b
Harun Mohamed <harunmohamed901@gmail.com>
2020-07-27 13:45:09 +0300

    recent_chats and algo for recommending people

--- bambiv3/routes.py
+++ bambiv3/routes.py
@@ -34,13 +34,18 @@ def home():
 def messages():
 	current_user.last_message_read_time = datetime.utcnow()
 	db.session.commit()
-	messages = current_user.messages_received.order_by(m.timestamp.desc())
+	messages_received = current_user.messages_received.order_by(m.timestamp.desc())
+	messages_sent = current_user.messages_sent.order_by(m.timestamp.desc())
 	recent_chats = list()
-	for message in messages:
+	for message in messages_received:
 		recent_chats.append(message.author)
+	for message in messages_sent:
+		recent_chats.append(message.recipient)
 	recent_chats = list(dict.fromkeys(recent_chats))
 	users = User.query.all()
-	return render_template('messages.html', messages=messages, users=users, recent_chats=recent_chats)
+	hour = datetime.now().hour
+	greeting = "Good morning" if 5<=hour<12 else "Good afternoon" if hour<18 else "Good evening"
+	return render_template('messages.html', users=users, greeting=greeting, recent_chats=recent_chats)
 
 @app.route('/m/<recipient>', methods=['GET', 'POST'])
 @login_required
@@ -77,7 +82,9 @@ def message(recipient):
 @login_required
 def discover():
 	users = User.query.all()
-	return render_template('discover.html', users=users, title='Discover')
+	hour = datetime.now().hour
+	greeting = "Good morning" if 5<=hour<12 else "Good afternoon" if hour<18 else "Good evening"
+	return render_template('discover.html', users=users, greeting=greeting, title='Discover')
 
 
 @app.route('/register', methods=['GET', 'POST'])
