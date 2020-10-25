f40561d8ffe52ef59bc0f619a24cf8d434d93567
harunmohamed <harunmohamed901@gmail.com>
2020-10-25 18:57:33 +0200

    fixing xss issues with the |safe keyword

--- dating/routes.py
+++ dating/routes.py
@@ -140,7 +140,7 @@ def login():
 	return render_template('login.html', title="Login", form=form)
 
 
-@app.route('/logout')
+@app.route('/logout', methods=['POST'])
 def logout():
 	logout_user()
 	return redirect('home')
